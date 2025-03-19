c1433cc98351fad443419cb6d5fde89b5d0be7bf
TomH <tom.heirich@epitech.eu>
2025-03-19 12:23:11 +0100

    [+] - Added temporary background for the visualizer. Added a blueprint texture for the visualizer background.

--- bonus/src/init_assets.c
+++ bonus/src/init_assets.c
@@ -7,22 +7,23 @@
 
 #include "../include/header.h"
 
-static void gender_reveal(sprite_t *guy, int i)
+static void gender_reveal(sprite_t *guy)
 {
     if (diceroll(0, 100) <= 8) {
         guy->rect.left = 175;
         guy->rect.width = 185;
     } else
         guy->rect.width = 175;
-    guy->color = color_from_hue(360 / DATA->robotcnt * i + GAME->hue, 255, 225);
+    sfSprite_setOrigin(guy->sprite,
+        (sfVector2f){guy->rect.width / 2.0, guy->rect.height / 2.0});
 }
 
-static void *init_sprites(void)
+static void *init_robots(void)
 {
     char *name;
     char *tmp;
 
-    for (int i = 0; i < DATA->robotcnt; i++) {
+    for (int i = 1; i <= DATA->robotcnt; i++) {
         name = malloc(sizeof(char) * (digitcount(i) + 2));
         if (name == NULL)
             return NULL;
@@ -30,34 +31,49 @@ static void *init_sprites(void)
         tmp = int_to_str(i);
         if (tmp == NULL)
             return OMNIFREE(name, 1);
-        strcpy(name, tmp);
+        strcpy(&name[1], tmp);
         OMNIFREE(tmp, 1);
-        make_sprite(name, "assets/guy.png", 100 + i * (400 / DATA->robotcnt), 100);
+        make_sprite(name, "assets/guy.png", 150 + (i - 1)
+            * (500 / (DATA->robotcnt - 1)), 285);
         if (get_sprite(name) == NULL)
             return OMNIFREE(name, 1);
-        gender_reveal(get_sprite(name), i);
+        gender_reveal(get_sprite(name));
         OMNIFREE(name, 1);
     }
 }
 
-static void *init_sounds(void)
+static int init_sprites(void)
 {
+    if (init_robots() == NULL)
+        return ERROR;
+    make_sprite("bg", "assets/bg.png", -100, -50);
+    get_sprite("bg")->color = color_from_hue(0, 255, 255, 255);
+    return SUCCESS;
 }
 
-static void *init_music(void)
+static int init_sounds(void)
 {
+    return SUCCESS;
 }
 
-void *init_assets(void)
+static int init_music(void)
+{
+    return SUCCESS;
+}
+
+int init_assets(void)
 {
     create_window(800, 600, "A-MAZING!");
     *get_clock() = sfClock_create();
-    if (init_sprites() == NULL)
-        return NULL;
-    if (init_sounds() == NULL)
-        return NULL;
-    if (init_music() == NULL)
-        return NULL;
+    if (init_cam() == NULL)
+        return ERROR;
+    if (init_sprites() == ERROR)
+        return ERROR;
+    if (init_sounds() == ERROR)
+        return ERROR;
+    if (init_music() == ERROR)
+        return ERROR;
+    return SUCCESS;
 }
 
 void destroy_assets(void)
@@ -77,5 +93,7 @@ void destroy_assets(void)
     destroy_music();
     destroy_clock();
     destroy_window();
-    free(DATA);
+    destroy_cam();
+    destroy_data();
+    free(GAME);
 }
