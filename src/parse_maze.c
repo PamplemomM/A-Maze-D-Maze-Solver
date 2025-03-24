a7b23369f0d9a6ff560678fddf440139930c3131
TomH <tom.heirich@epitech.eu>
2025-03-24 12:15:57 +0100

    [+] - Added tunnels parsing. The program can now read tunnels and link rooms together according to those tunnels. Also, a 'tunnel_t' linked list has been made, to keep track of them.

--- src/parse_maze.c
+++ src/parse_maze.c
@@ -7,32 +7,77 @@
 #include "../include/struct.h"
 #include "../include/header_amazed.h"
 
+void free_maze(maze_t *maze)
+{
+    room_t *current = maze->rooms;
+    tunnel_t *tunnel = maze->tunnels;
+    void *tmp = NULL;
+    int i = 0;
+
+    while (current != NULL) {
+        tmp = current->next;
+        OMNIFREE(current->name, 1);
+        if (current->links != NULL)
+            OMNIFREE(current->links, 1);
+        OMNIFREE(current, 1);
+        current = tmp;
+    }
+    while (tunnel != NULL) {
+        tmp = tunnel->next;
+        OMNIFREE(tunnel, 1);
+        tunnel = tmp;
+    }
+    OMNIFREE(maze, 1);
+}
+
+static int read_room(char *line, maze_t *maze)
+{
+    static int end_or_start = 0;
+
+    if (line[0] == '#' && my_strncmp(line, "##start", 7) == 0)
+        end_or_start = 1;
+    if (line[0] == '#' && my_strncmp(line, "##end", 5) == 0)
+        end_or_start = 2;
+    if (line[0] == '#')
+        return SUCCESS;
+    return parse_room(maze, line, &end_or_start);
+}
+
+static int read_tunnel(char *line, maze_t *maze)
+{
+    if (line[0] == '#')
+        return SUCCESS;
+    return parse_tunnel(maze, line);
+}
+
+static void nullify_maze(maze_t *maze)
+{
+    maze->rooms = NULL;
+    maze->start = NULL;
+    maze->end = NULL;
+    maze->tunnels = NULL;
+}
+
 maze_t *parse_maze(void)
 {
     char *line = NULL;
     size_t len = 0;
-    int end_or_start = 0;
     maze_t *maze = malloc(sizeof(maze_t));
-    
+
     if (maze == NULL)
         return NULL;
     if (getline(&line, &len, stdin) == -1)
         return OMNIFREE(maze, 1);
     maze->nb_robots = my_getnbr(line);
-    maze->rooms = NULL;
-    maze->start = NULL;
-    maze->end = NULL;
-    while (getline(&line, &len, stdin) != -1) {     
-        if (line[0] == '#' && my_strncmp(line, "##start", 7) == 0)
-            end_or_start = 1;
-        if (line[0] == '#' && my_strncmp(line, "##end", 5) == 0)
-            end_or_start = 2;
-        if (line[0] == '#')
-            continue;
-        if (parse_room(maze, line, &end_or_start) == 0)
+    nullify_maze(maze);
+    while (getline(&line, &len, stdin) != -1) {
+        if (read_room(line, maze) != SUCCESS)
             break;
     }
+    do {
+        if (read_tunnel(line, maze) != SUCCESS)
+            break;
+    } while (getline(&line, &len, stdin) != -1);
     OMNIFREE(line, 1);
     return maze;
 }
-// Il reste juste à implémenter le parsing des tunnels entre les rooms
