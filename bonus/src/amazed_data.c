372e2bbf458f2fd05853850a6972272801e87abc
TomH <tom.heirich@epitech.eu>
2025-03-28 14:08:34 +0100

    [VIS] [+] - Added error handling and error messages to moves parsing. Self explanatory.

--- bonus/src/amazed_data.c
+++ bonus/src/amazed_data.c
@@ -19,18 +19,21 @@ static int parse_move(char *line, int offset)
     static int move_id = 0;
     char *robot_name = get_name(line, &offset, " -\n");
     char *room_name = get_name(line, &offset, " -\n");
-    int robot_id = my_getnbr(&robot_name[1]);
     room_t *room = get_room(room_name, MAZE);
+    int robot_id;
 
-    printf("P%d-%s ", my_getnbr(&robot_name[1]), room_name);
+    if (robot_name == NULL || room_name == NULL)
+        return give_up("Move with invalid robot or room.", &MAZE);
+    robot_id = my_getnbr(&robot_name[1]);
     OMNIFREE(robot_name, 1);
     OMNIFREE(room_name, 1);
-    if (robot_id <= 0 || robot_id > MAZE->nb_robots || room == NULL)
-        return ERROR;
+    if (robot_id <= 0 || robot_id > MAZE->nb_robots)
+        return give_up("Move with non-existent robot.", &MAZE);
+    if (room == NULL)
+        return give_up("Move to non-existent room.", &MAZE);
     make_move(robot_id, room, move_id, MAZE);
     if (line[offset] == 'P')
         return parse_move(line, offset);
-    printf("\n");
     move_id++;
     return SUCCESS;
 }
@@ -39,25 +42,27 @@ static int read_moves(void)
 {
     char *line = NULL;
     size_t len = 0;
+    int cnt = 0;
 
-    while (getline(&line, &len, stdin) != -1) {
+    while (!end_of_file(getline(&line, &len, stdin), &line)) {
         if (parse_move(line, 0) == ERROR) {
             OMNIFREE(line, 1);
             return ERROR;
         }
+        cnt++;
     }
+    if (cnt == 0 && line == NULL)
+        return give_up("No moves.", &MAZE);
     OMNIFREE(line, 1);
     return SUCCESS;
 }
 
 int read_maze(void)
 {
-    MAZE = parse_maze();
+    MAZE = parse_maze(1);
     if (MAZE == NULL)
         return ERROR;
-    if (read_moves() == ERROR) {
-        free_maze(&MAZE);
+    if (read_moves() == ERROR)
         return ERROR;
-    }
     return SUCCESS;
 }
