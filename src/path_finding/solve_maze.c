86e917230d56263b17ea26574407efb32b2657f4
PamplemomM <tom.marmoz@epitech.eu>
2025-03-31 12:03:04 +0200

    [🚀] - Addition: More optimisation in 'find_robot_move' and started the function for multiple moves. The function will be able to move multiple robots at the same time.

--- src/path_finding/solve_maze.c
+++ src/path_finding/solve_maze.c
@@ -11,6 +11,25 @@ int solve_maze(maze_t *maze)
 {
     return SUCCESS;
 }
+/*
+static int find_mult_robot_move(room_t *current, maze_t *maze, int robot)
+{
+    tunnel_t *tunnel = NULL;
+
+    if (current == NULL || current->links == NULL)
+        return SUCCESS;
+    for (int i = 0; current->links[i] != NULL; i++) {
+        tunnel = get_tunnel(current, current->links[i], maze);
+        if (tunnel == NULL)
+            continue;
+        if (tunnel->val == 1) {
+            mini_printf("P%d-%s\n", robot, current->links[i]->name);
+            find_mult_robot_move(current->links[i], maze, robot);
+            break;
+        }
+    }
+    return SUCCESS;
+}*/
 
 static int find_robot_move(room_t *current, maze_t *maze, int robot)
 {
@@ -25,6 +44,7 @@ static int find_robot_move(room_t *current, maze_t *maze, int robot)
         if (tunnel->val == 1) {
             mini_printf("P%d-%s\n", robot, current->links[i]->name);
             find_robot_move(current->links[i], maze, robot);
+            break;
         }
     }
     return SUCCESS;
