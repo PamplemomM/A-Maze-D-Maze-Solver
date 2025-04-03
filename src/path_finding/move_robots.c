24eed50ae9b6660d0f56774ab57694cfdf7bc8c4
PamplemomM <tom.marmoz@epitech.eu>
2025-04-03 15:04:26 +0200

    [🚀] - Addition: Function for duplicating the path into a char *.

--- src/path_finding/move_robots.c
+++ src/path_finding/move_robots.c
@@ -7,19 +7,32 @@
 
 #include "../../include/header_amazed.h"
 
-int move_robots(path_t **good_path, maze_t *maze)
+static int dup_pathlist(path_t **good_path, maze_t *maze)
 {
     path_t *pathlist = *good_path;
     int size = 0;
-    char **path
+    char **path = NULL;
 
     while (pathlist != NULL) {
         size++;
-        for (int i = 1; i < maze->nb_robots + 1; i++)
-            mini_printf("P%d-%s\n", i, path->name);
         pathlist = pathlist->next;
     }
     path = malloc(sizeof(char *) * (size + 1));
+    if (path == NULL)
+        return ERROR;
+    
+    return SUCCESS;
+}
+
+int move_robots(path_t **good_path, maze_t *maze)
+{
+    path_t *pathlist = *good_path;
+
+    while (pathlist != NULL) {
+        for (int i = 1; i < maze->nb_robots + 1; i++)
+            mini_printf("P%d-%s\n", i, pathlist->name);
+        pathlist = pathlist->next;
+    }
     return SUCCESS;
 }
 
