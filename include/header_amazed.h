8e439fd274b0f0294ab524390d0d4cee05d82898
PamplemomM <tom.marmoz@epitech.eu>
2025-04-03 15:15:11 +0200

    [🛠️] - Refactor: Changed the name of the file 'move_robots' into 'display_robot_move' because this name was already took in the viewer.

--- include/header_amazed.h
+++ include/header_amazed.h
@@ -94,14 +94,14 @@ int parse_tunnel(maze_t **maze, char *line);
 
 // --------- PATH_FINDING FOLDER ---------
 
+// --- display_robots_move.c ---
+int display_robots_move(path_t **good_path, maze_t *maze);
+
 // --- make_move.c ---
 move_t *make_move(int robot_id, room_t *dest, int move_id, maze_t *maze);
 
 // --- find_moves.c ---
 move_t *find_moves(maze_t *maze, int current_id);
-
-// --- move_robots.c ---
-int display_robots_move(path_t **good_path, maze_t *maze);
 
 // --- solve_maze.c ---
 int solve_maze(maze_t *maze);
