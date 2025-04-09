a6b95511972c1ad45ee5faab93f6d078ce8cfdb6
PamplemomM <tom.marmoz@epitech.eu>
2025-04-09 17:26:26 +0200

    [🚀] - Addition: function push_path_front. This function will push the path into the front of the function.

--- src/path_finding/solve_maze.c
+++ src/path_finding/solve_maze.c
@@ -41,6 +41,11 @@ static int find_mult_robot_move(room_t *current, maze_t *maze, int robot)
     return SUCCESS;
 }
 
+static int push_path_front(path_t **node, path_t *new_path)
+{
+    return SUCCESS;
+}
+
 static int push_path_back(path_t **node, path_t *new_path)
 {
     return SUCCESS;
