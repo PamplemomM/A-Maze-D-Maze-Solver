fda7e3087f5d21710c0672509afd50a8df76422c
PamplemomM <tom.marmoz@epitech.eu>
2025-04-09 17:07:32 +0200

    [🗑️] - Removed: Removed the old push_back in the linked list. Self Explanatory.

--- src/path_finding/solve_maze.c
+++ src/path_finding/solve_maze.c
@@ -55,14 +55,6 @@ int add_new_path(path_t **node, char *room)
     new_path->room = NULL;
     new_path->next = *node;
     *node = new_path;
-    /*new_path->next = NULL;
-    if (current == NULL) {
-        *node = new_path;
-        return SUCCESS;
-    }
-    while (current->next != NULL)
-        current = current->next;
-    current->next = new_path;*/
     return SUCCESS;
 }
 
