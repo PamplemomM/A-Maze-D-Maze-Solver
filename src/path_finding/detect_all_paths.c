006e741bcfea904c093a15ab3976602fb050b558
PamplemomM <tom.marmoz@epitech.eu>
2025-04-09 11:14:36 +0200

    [🚀] - Addition: find_all_move function. This function will find all the moves from a path and stores it into a path_t **path.

--- src/path_finding/detect_all_paths.c
+++ src/path_finding/detect_all_paths.c
@@ -21,6 +21,26 @@ int free_pathlist(pathlist_t *paths)
     return SUCCESS;
 }
 
+static int find_all_moves(room_t *current, maze_t *maze,
+    path_t **path)
+{
+    tunnel_t *tunnel = NULL;
+
+    if (current == NULL || current->links == NULL)
+        return SUCCESS;
+    for (int i = 0; current->links[i] != NULL; i++) {
+        tunnel = get_tunnel(current, current->links[i], maze);
+        if (tunnel == NULL || tunnel->val == 1)
+            continue;
+        tunnel->val = 1;
+        if (find_robot_move(current->links[i], maze) == SUCCESS) {
+            add_new_path(path, current->links[i]->name);
+            return SUCCESS;
+        }
+    }
+    return ERROR;
+}
+
 pathlist_t *find_allpath(maze_t *maze)
 {
     pathlist_t *paths = NULL;
