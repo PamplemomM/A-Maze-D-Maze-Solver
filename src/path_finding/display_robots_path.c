a19323bbf9b46f03e22756d56bdb26cc437abe6b
PamplemomM <tom.marmoz@epitech.eu>
2025-04-09 19:55:43 +0200

    [🚀] - Addition: Comment for later me (this is the way). Self Explanatory.

--- src/path_finding/display_robots_path.c
+++ src/path_finding/display_robots_path.c
@@ -15,3 +15,37 @@ int calculate_paths_proportion(pathlist_t *paths, maze_t *maze)
         return ERROR;
     return SUCCESS;
 }
+/*
+
+Step 1:
+Trier tous les paths dans l'ordre croissant.
+Step 2:
+Launch every robots in a path is nb_robots >= tpath (length path).
+Step 3:
+Repeat.
+
+Imagine a maze :
+  A-B
+ /   \
+S--C--E
+|\   /|
+D  H  G
+ \   /
+  E-F
+
+Paths :
+S-C-E -> 2
+S-H-E -> 2
+S-A-B-E -> 3
+S-D-E-F-G-E -> 5
+
+Si il y a 3 robots, tu envoie dans les nb_robots (trois) premiers
+qui sont triees si tpath (longueur du path) est inferieure ou egale au nombre de robots.
+Donc un dans SCE, un dans SHE et un dans SABE.
+
+Si il y a 7 robots, ca va donc en envoyer un dans chaque path au premier tour,
+et au deuxieme tour il n'y aura plus que 3 robots, donc ca va envoyer que
+dans ceux <= 4 donc SCE, SHE ou SABE. Donc au total, un seul partira dans
+SDEFGE car c'est le plus optimise.
+
+*/
