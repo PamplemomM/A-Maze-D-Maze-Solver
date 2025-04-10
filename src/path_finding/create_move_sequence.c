619835bc3aee16897c9dbff398e6d65d25c6f94a
PamplemomM <tom.marmoz@epitech.eu>
2025-04-10 18:48:57 +0200

    [🚀] - Addition: move_robot_untilend function. This function will give a move to a robot until the end.

--- src/path_finding/create_move_sequence.c
+++ src/path_finding/create_move_sequence.c
@@ -7,3 +7,9 @@
 
 #include "../../include/header_amazed.h"
 
+move_t *make_move(int robot_id, room_t *dest, int move_id, maze_t *maze);
+
+int move_robot_untilend(path_t **path, int start_id, int robot_id, maze_t *maze)
+{
+    return SUCCESS;
+}
