53dccc56c88cf646b136116efabf64a93773f5d3
PamplemomM <tom.marmoz@epitech.eu>
2025-04-10 19:18:37 +0200

    [🔧] - Refactor: Finished the function move_robot_untilend. This function will move every robots until the end depending on their given path.

--- src/path_finding/create_move_sequence.c
+++ src/path_finding/create_move_sequence.c
@@ -9,7 +9,17 @@
 
 move_t *make_move(int robot_id, room_t *dest, int move_id, maze_t *maze);
 
-int move_robot_untilend(path_t **path, int start_id, int robot_id, maze_t *maze)
+int move_robot_untilend(pathlist_t **path, int start_id, int robot_id, maze_t *maze)
 {
+    pathlist_t *currentlist = path;
+    path_t *current = currentlist->path;
+    int id = start_id;
+
+    while (current != NULL) {
+        mini_printf("Making the move for robot %d during %d in %s", roboyt_id, id, current->name);
+        make_move(robot_id, current->room, id, maze);
+        id++;
+        current = current->next;
+    }
     return SUCCESS;
 }
