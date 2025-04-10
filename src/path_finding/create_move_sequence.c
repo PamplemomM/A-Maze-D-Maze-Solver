483f1d29f31ebef0185fdf04c9400e9878af0eab
PamplemomM <tom.marmoz@epitech.eu>
2025-04-10 18:43:03 +0200

    [🚀] - Addition: Create move sequence file. This file will contains the functions that will create the moves sequence.

---
+++ src/path_finding/create_move_sequence.c
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2025
+** create_move_sequence.c
+** File description:
+** The file that will help creating the move sequence.
+*/
+
+#include "../../include/header_amazed.h"
+
