04f903a97548e5194621ccb16f27ede84145b5f4
PamplemomM <tom.marmoz@epitech.eu>
2025-04-10 14:55:10 +0200

    [🚀] - Addition: Shortes paths in the calcul.

--- src/path_finding/display_robots_path.c
+++ src/path_finding/display_robots_path.c
@@ -46,13 +46,15 @@ int find_path_lower(pathlist_t **paths)
 int calculate_paths_proportion(pathlist_t **paths, maze_t *maze)
 {
     pathlist_t *current = *paths;
+    pathlist_t *shortest = get_shortest_pathlist(paths);
     pathlist_t *tmp = NULL;
 
     if (*paths == NULL || maze->nb_robots == 1)
         return SUCCESS;
     while (current != NULL) {
         tmp = current->next;
-        if (maze->nb_robots <= current->length + current->lower)
+        if (MAX(maze->nb_robots, shortest->length) <= current->length +
+            current->lower)
             delete_path_fromlist(current, paths);
         current = tmp;
     }
