f70fce2e0c21755b24e18b088c9355415013ed4e
PamplemomM <tom.marmoz@epitech.eu>
2025-04-10 20:17:15 +0200

    [🚀] - Addition: Sort path function. This function will sort the path in decroissant order.

--- src/path_finding/sort_path.c
+++ src/path_finding/sort_path.c
@@ -7,3 +7,19 @@
 
 #include "../../include/header_amazed.h"
 
+static int sort_path(pathlist_t **paths)
+{
+    pathlist_t *list = NULL;
+    pathlist_t *current = *paths;
+    pathlist_t *next;
+
+    while (current != NULL) {
+        next = current->next;
+        current->next = NULL;
+        insert_list(&list, current);
+        current = next;
+    }
+    *paths = list;
+    return SUCCESS;
+}
+
